7eb140ccd99fa82ca600afd8ca40a3ddfe96e165
kaverikhaneja <kaveri.khaneja@accenture.com>
2021-10-14 09:01:39 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: kaverikhaneja <kaveri.khaneja@accenture.com>

--- examples/supplychain-app/besu/smartContracts/createKeyFile.js
+++ examples/supplychain-app/besu/smartContracts/createKeyFile.js
@@ -3,6 +3,6 @@ const Web3 = require('web3')
 // Web3 initialization (should point to the JSON-RPC endpoint)
 const web3 = new Web3(new Web3.providers.HttpProvider('http://m-peer1.bes.dev2.aws.blockchaincloudpoc-develop.com:8590'))
 
-var V3KeyStore = web3.eth.accounts.encrypt("0x36b36e3f9170d1e4b46d4cd3b82e6789e5e3733f7d67fbb221547c7b8b67a1c3", "Password");
+var V3KeyStore = web3.eth.accounts.encrypt("0x8f5fe96c21547263ac39712f9b88cfe06a9a68c54ec12a92035727777bfc48d5", "Password");
 console.log(JSON.stringify(V3KeyStore));
 process.exit();
