1be4e0fd4ae1dabfe3e54f6a5f420830fba2890d
kaverikhaneja <kaveri.khaneja@accenture.com>
2021-10-27 11:31:58 +0000

    [ci skip] Pushing deployment files for namespace and service accounts
    Signed-off-by: kaverikhaneja <kaveri.khaneja@accenture.com>

--- examples/supplychain-app/besu/express_nodeJS/web3services.js
+++ examples/supplychain-app/besu/express_nodeJS/web3services.js
@@ -1,6 +1,7 @@
 var Web3 = require("web3");
 var {productABI} = require("./ABI/productABI");
 var {productContractAddress,quorumServer, ganacheServer, nodeIdentity, nodeOrganization, nodeOrganizationUnit, nodeSubject,protocol} = require("./config");
+var Contract = require('web3-eth-contract');
 
 // Smart contract address
 const fromAddress = nodeIdentity;
@@ -18,10 +19,12 @@ console.log(fromAddress+","+fromNodeSubject);
 
 console.log(protocol);
 
-web3 = new Web3(new Web3.providers.HttpProvider(ganacheServer));
+const web3 = new Web3(new Web3.providers.HttpProvider(ganacheServer));
+Contract.setProvider(ganacheServer);
 
 //instantiate the product smartcontract 
 let productContract = new web3.eth.Contract(productABI, productContractAddress);
+// var contract = new Contract(productABI, productContractAddress);
 
 module.exports = {
     productContract,
