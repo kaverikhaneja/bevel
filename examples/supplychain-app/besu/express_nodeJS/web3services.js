a1e7f7614b7a7fb3b4980005ca7885fbef24cc85
kaverikhaneja <kaveri.khaneja@accenture.com>
2021-11-03 10:01:29 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: kaverikhaneja <kaveri.khaneja@accenture.com>

--- examples/supplychain-app/besu/express_nodeJS/web3services.js
+++ examples/supplychain-app/besu/express_nodeJS/web3services.js
@@ -1,4 +1,5 @@
 var Web3 = require("web3");
+const Web3Quorum = require('web3js-quorum');
 var {productABI} = require("./ABI/productABI");
 var {productContractAddress,quorumServer, ganacheServer, nodeIdentity, nodeOrganization, nodeOrganizationUnit, nodeSubject,protocol} = require("./config");
 
@@ -21,10 +22,12 @@ console.log(fromAddress+","+fromNodeSubject);
 
 console.log(protocol);
 
-const web3 = new Web3(new Web3.providers.HttpProvider(ganacheServer));
+// const web3 = new Web3(new Web3.providers.HttpProvider(ganacheServer));
+const web3quorum = new Web3Quorum(new Web3(ganacheServer));
+
 
 //instantiate the product smartcontract 
-let productContract = new web3.eth.Contract(productABI, productContractAddress);
+let productContract = new web3quorum.eth.Contract(productABI, productContractAddress);
 console.log("test");
 console.log(productContract);
 
